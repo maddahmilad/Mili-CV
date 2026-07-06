02f1021b903073d0a0b1d79399a23008adae7557
maddahmilad <120320784+maddahmilad@users.noreply.github.com>
2026-07-06 16:50:13 +0330

    Add files via upload

---
+++ resume-data.js
@@ -0,0 +1,145 @@
+const myResumeData = {
+//////////////////////////////////////////////////// (Experience I) ///////////////////////////////////////////////////////////////
+    "experience": [
+        {
+            logo: "resumeimage/up.png", // اگر عکس لوگو داشتی آدرسش رو اینجا بذار (مثلا "tapsi.png")، اگر خالی باشه یک کادر سفید میاد
+            title: "Graphic Designer",
+            company: "Up",
+            date: "Nov 2023 - Present · 2y & 6 mos",
+            description: [
+                        "Skills: Adobe Illustrator · Adobe Photoshop . Figma"
+                         ]
+        },
+        // می‌تونی موقعیت شغلی دوم رو همینجا اضافه کنی:
+        {
+            logo: "resumeimage/parla.png",
+            title: "Product Designer",
+            company: "Perla",
+            date: "Feb 2021 - Jul 2021 · 6 mos",
+            description: [
+                        ". Product packaging design,modeling and mockup packaging design,product shelf design",
+                        ". Product shelf design"
+                         ]
+        },
+        {
+            logo: "resumeimage/venedik.png",
+            title: "Establisher",
+            company: "Venedik",
+            date: "Jan 2020 - Dec 2020 · 6 mos",
+            description: [
+                        ". Communicating with designers and developer",
+                        ". Launch new and improved integrations, test their performance, and iterate quickly",
+                        ". Suggesting product lists and creative ideas to enhance product visibility and sales.",
+                        ". Analyze customer needs and behavior to extract trends and insights."
+                         ]
+        }
+    ],
+////////////////////////////////////////////////////////// (Experience III) /////////////////////////////////////////////////////////
+    // اطلاعات لوله تحصیلات 
+    "education": [
+        {
+            logo: "resumeimage/tapsifood.png",
+            title: " Product-Led Business Development",
+            company: "Tapsi Food",
+            date: "Nov 2023 - Present · 2y & 10 mos ",
+            description: [
+                        "In this dual-impact role, I orchestrated the strategic growth of the platform by managing the partnership ecosystem and optimizing the product’s financial performance through data-driven decision-making:",
+                        ". Strategic Partnership Ecosystem & Negotiation: Managed the end-to-end lifecycle of third-party collaborations, from technical due diligence to high-stakes commercial negotiations, such as securing new map and SMS providers which resulted in a 7% reduction in infrastructure overheads.",
+                        ". Revenue Engineering & Unit Economics: Directed FP&A and cost estimation for tech and product teams, leading to a restructured delivery payout model that shifted from fixed to performance-based, achieving a 11% reduction in Cost per Delivery (CPD).",
+                        ". Contractual Strategy & Risk Management: Led the review and alignment of commercial contracts with product roadmaps, negotiating enhanced Service Level Agreements (SLAs) for payment and POS integrations that secured operational cash flow stability.",
+                        ". Product-Led Growth & Integration: Spearheaded high-leverage integrations with major restaurant chain POS systems, successfully reducing partner onboarding time.",
+                        ". Operational Efficiency & Technical Optimization: Optimized marketing and tech spend by identifying underutilized third-party tools and replacing them with other solutions, saving of millions."
+                         ]
+        }
+    ],
+////////////////////////////////////////////////////////// (About Me) /////////////////////////////////////////////////////////
+    // 
+    "skills": [
+        {
+            logo: "resumeimage/me.png",
+            title: " About Me",
+            company: "",
+            date: "Birthday : Mar 1995",
+            description: ["I started in product through industrial design — learning to balance form, function, and user needs. That mindset still shapes how I build digital experiences that solve real problems. Over the years, I’ve worked across teams and industries to create scalable, user-centered solutions — from delivery flows to third-party integrations. I love turning business goals into product strategies, collaborating with cross-functional teams, and building features people actually use. Always learning, always iterating. Let’s connect if you’re into product , design, or just curious."
+                         ]
+        }
+    ],
+/////////////////////////////////////////////////////////// (Experience II) ////////////////////////////////////////////////////////
+    // 
+    "projects": [
+        {
+            logo: "resumeimage/tether land.png", // اگر عکس لوگو داشتی آدرسش رو اینجا بذار (مثلا "tapsi.png")، اگر خالی باشه یک کادر سفید میاد
+            title: "Associate Product Manager",
+            company: "Tether Land",
+            date: "Feb 2023 - Jul 2023 · 6 mos",
+            description: [
+                        "Tetherland is a leading crypto exchange platform in Iran, offering secure and user-friendly services for trading and managing digital assets. As Associate Product Manager, I led the product development of “Ghalak”, a staking feature that allows users to lock their crypto assets for fixed or flexible durations and earn network rewards."
+                         ]
+        },
+        // می‌تونی موقعیت شغلی دوم رو همینجا اضافه کنی:
+        {
+            logo: "resumeimage/tether land.png",
+            title: "Graphic Designer ",
+            company: "Tether Land",
+            date: "Feb 2022 - Jan 2023 · 1 yr",
+            description: [
+                        "As Graphic Designer at Tetherland, I contributed to the brand’s visual identity and marketing efforts through creative design and campaign execution."
+                         ]
+        },
+        {
+            logo: "resumeimage/toogeder app.png",
+            title: "Associate Product Manager",
+            company: "Toogeder App (Freelance Project)",
+            date: "Aug 2023 - oct 2023 · 3 mos",
+            description: [
+                         "At Toogeder, a social platform focused on authentic event-based connections, I helped shape the product experience and drive execution across core features."
+                         ]
+        },
+        {
+            logo: "resumeimage/new world.png",
+            title: "Project Manager ",
+            company: "New world (Freelance Project)",
+            date: "May 2023 - Jul 2023 · 3 mos",
+            description: [
+                        "At New World, a gaming platform merging entertainment with decentralized technologies, I led a cross-functional product initiative focused on secure, transparent, and rewarding user experiences."
+                         ]
+        }
+    ], 
+/////////////////////////////////////////////////////////// (Skils) ////////////////////////////////////////////////////////
+    "contact": [
+        {
+            logo: "", //
+            title: " Hard Skils",
+            company: "",
+            date: "",
+            description: [
+                         ". Data-Driven Decision Making",
+                         ". SQL and Analytics Tools",
+                         ". Data Analysis and Metrics",
+                         ". Stakeholder Managemen",
+                         ". product lifecycle",
+                         ". lean development",
+                         ". Technical Knowledge",
+                         ". Ai"
+                         ]
+        },
+        {
+            logo: "", //
+            title: " Soft Skils",
+            company: "",
+            date: "",
+            description: [
+                         ". Strategic Thinking",
+                         ". Problem Solving",
+                         ". Strong Communication Skills",
+                         ". Cross-Functional Collaboration",
+                         ". Adaptability & Fast Learning",
+                         ". Contract Negotiation and Review",
+                         ". Third-Party Evaluation"
+                         ]
+        }
+
+    ]
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+   // "about": []
+};
